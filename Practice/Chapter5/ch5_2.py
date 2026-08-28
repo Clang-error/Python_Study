66f2801fc5a71a636ddc360ab4538a633797f10d
Clang_error <wndwnddoq@gmail.com>
2026-08-28 23:49:13 +0900

    연습문제 풀이

---
+++ Practice/Chapter5/ch5_2.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def __init__(self):
+        self.value = 0
+    def add(self, val):
+        self.value += val
+
+class MaxLimitCalulator(Calculator):
+    def add(self, val):
+        super().add(val)
+        if self.value > 100:
+            self.value = 100
+
+cal = MaxLimitCalulator()
+cal.add(50)
+cal.add(60)
+
+print(cal.value)
